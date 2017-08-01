98e84971c7e7dbc508640ef1d8747770a19f43f5
matt <w20031213@gmail.com>
2017-08-01 16:54:12 +0800

    test

---
+++ src/main/java/com/matt/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.matt.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+//@Configuration
+//@EnableWebMvc
+//public class WebConfiguration extends WebMvcConfigurerAdapter {
+//
+//	private final Logger logger = LoggerFactory.getLogger(WebConfiguration.class);
+//
+//
+//}
